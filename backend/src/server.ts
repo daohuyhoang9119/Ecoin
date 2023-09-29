2297505fa4454f054c754492d28a9dd10957f8e3
HoangDH <daohuyhoang9119@gmail.com>
2023-09-29 12:38:41 +0700

    :package: Add folder, packages for project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,34 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import path from 'path';
+import morgan from 'morgan';
+import compression from 'compression';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
 const port = process.env.PORT || 8888;
 
+//init middlewares
+// Use Morgan middleware
+app.use(morgan('dev'));
+// app.use(morgan('combined'));
+
+app.use(
+  compression({
+    threshold: 0,
+    level: 6
+  })
+);
+
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.status(200).json({
+    msg: 'Welcome từ đây'
+  });
+});
+app.get('/a', (req, res) => {
+  res.status(200).json({
+    msg: 'từ a'
+  });
 });
 
 app.listen(port, () => {
